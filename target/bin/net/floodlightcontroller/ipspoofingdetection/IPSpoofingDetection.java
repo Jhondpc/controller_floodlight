be9df7eb7d196bba4c0e9bce47b02974fb490d0f
Jhondpc <a20196137@pucp.edu.pe>
2023-12-04 21:23:20 -0500

    a

--- target/bin/net/floodlightcontroller/ipspoofingdetection/IPSpoofingDetection.java
+++ target/bin/net/floodlightcontroller/ipspoofingdetection/IPSpoofingDetection.java
@@ -1,18 +1,15 @@
 package net.floodlightcontroller.ipspoofingdetection;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
-import net.floodlightcontroller.core.IFloodlightProviderService;
-import org.projectfloodlight.openflow.protocol.OFMessage;
-import org.projectfloodlight.openflow.protocol.OFType;
-import org.projectfloodlight.openflow.types.IPv4Address;
-import org.projectfloodlight.openflow.types.MacAddress;
-
 import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
@@ -21,8 +18,19 @@ import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.util.FlowModUtils;
+import org.projectfloodlight.openflow.protocol.*;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActions;
+import org.projectfloodlight.openflow.protocol.instruction.OFInstruction;
+import org.projectfloodlight.openflow.protocol.instruction.OFInstructions;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.*;
 
 public class IPSpoofingDetection implements IOFMessageListener, IFloodlightModule {
+
+    private static final int ICMP_PROTOCOL = IpProtocol.ICMP.getIpProtocolNumber();
 
     protected IFloodlightProviderService floodlightProvider;
     protected Map<MacAddress, Set<IPv4Address>> macToIpMapping;
@@ -83,6 +91,9 @@ public class IPSpoofingDetection implements IOFMessageListener, IFloodlightModul
             if (macToIpMapping.get(sourceMAC).size() > 1) {
                 System.out.println("Potential IP Spoofing detected. MAC: " + sourceMAC.toString() +
                         " IP: " + sourceIP.toString() + " Switch: " + sw.getId().toString());
+
+                // Mitigar el ataque bloqueando el tráfico ICMP
+                addFlowEntry(sw, sourceIP);
             }
         }
 
@@ -94,5 +105,38 @@ public class IPSpoofingDetection implements IOFMessageListener, IFloodlightModul
             return ((IPv4) eth.getPayload()).getSourceAddress();
         }
         return IPv4Address.NONE;
+    }
+
+    private void addFlowEntry(IOFSwitch sw, IPv4Address sourceIP) {
+        Match match = sw.getOFFactory().buildMatch()
+                .setExact(MatchField.ETH_TYPE, EthType.IPv4)
+                .setExact(MatchField.IPV4_SRC, sourceIP)
+                .setExact(MatchField.IP_PROTO, IpProtocol.ICMP)
+                .build();
+
+        ArrayList<OFAction> actions = new ArrayList<>();
+        OFActions actionsBuilder = sw.getOFFactory().actions();
+
+        // Acción para descartar (DROP) el paquete ICMP
+        OFAction drop = actionsBuilder.buildOutput().setPort(OFPort.CONTROLLER).build();
+        actions.add(drop);
+
+        ArrayList<OFInstruction> instructions = new ArrayList<>();
+        OFInstructions instructionsBuilder = sw.getOFFactory().instructions();
+
+        // Instrucción para aplicar las acciones (en este caso, descartar el paquete ICMP)
+        OFInstruction applyActions = instructionsBuilder.buildApplyActions().setActions(actions).build();
+        instructions.add(applyActions);
+
+        sw.getOFFactory().buildFlowAdd()
+                .setMatch(match)
+                .setInstructions(instructions)
+                .setPriority(FlowModUtils.PRIORITY_MAX)
+                .setBufferId(OFBufferId.NO_BUFFER)
+                .setHardTimeout(0)
+                .setIdleTimeout(0)
+                .setFlags(Collections.emptySet())
+                .setCookie(U64.of(0))
+                .build();
     }
 }
